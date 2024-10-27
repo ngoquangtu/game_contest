4047fa628f1829e658bab534e4134f5fc83520e2
ngoquangtu <ngoquangtuhd@gmail.com>
2024-10-27 22:14:52 +0700

    update init

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float moveSpeed = 5f; // Tốc độ di chuyển của nhân vật
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        // Lấy Rigidbody2D để điều khiển vật lý
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Nhận đầu vào từ bàn phím (phím WASD hoặc mũi tên)
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        // Di chuyển nhân vật theo hướng đầu vào và tốc độ di chuyển
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI timerText;
+    public TextMeshProUGUI levelText;
+
+    // Game variables
+    private int score = 0;
+    public float timer = 60f;
+    private int level = 1;
+    public static GameManager Instance { get; private set; }
+
+    void Start()
+    {
+        UpdateScoreText();
+        UpdateTimerText();
+        UpdateLevelText();
+    }
+    private void Awake()
+    {
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this; 
+    }
+    void Update()
+    {
+        // Update Timer
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            UpdateTimerText();
+
+            if (timer <= 0)
+            {
+                EndGame();
+            }
+        }
+    }
+
+    public void AddScore(int points)
+    {
+        score += points;
+        UpdateScoreText();
+    }
+
+    public void NextLevel()
+    {
+        level++;
+        timer += 10f; // Tăng thời gian khi lên cấp (tuỳ chỉnh)
+        UpdateLevelText();
+        UpdateTimerText();
+    }
+
+    private void UpdateScoreText()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    private void UpdateTimerText()
+    {
+        timerText.text = "Time: " + Mathf.Ceil(timer).ToString(); 
+    }
+
+    private void UpdateLevelText()
+    {
+        levelText.text = "Level: " + level;
+    }
+
+    private void EndGame()
+    {
+        Debug.Log("Game Over!");
+    }
+}
